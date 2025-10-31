a082eec22647ab382c4baf25bb1c0e0f3d39408b
alextomasella <alextomasella249@gmail.com>
2025-10-31 13:06:24 -0500

    added the endpoint url and made an async function structure.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("script.js loaded");
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=OVHpSWJMuftkrJVmp4VLxAdNRdGAHvPP&q=Halloween&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+console.log(endpoint);
+
+async function getGifs() {
+    const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=OVHpSWJMuftkrJVmp4VLxAdNRdGAHvPP&q=Halloween&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+    const data = await response.json();
+    const images = data.data.map(gif => gif.images.original.url);
+    console.log(images);
+
+    const output = document.getElementById("gif-output");
+    output.innerHTML = "";
+}
+
